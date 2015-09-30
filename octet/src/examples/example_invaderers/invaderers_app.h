7eaa5eadddc9292e074ca4600a3d0f11b14f51d4
KunAn, Li <supersonicman1990@gmail.com>
2015-09-30 15:51:35 +0100

    find number of lives

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -474,7 +474,7 @@ namespace octet {
       bombs_disabled = 50;
       invader_velocity = 0.01f;
       live_invaderers = num_invaderers;
-      num_lives = 3;
+      num_lives = 999;
       game_over = false;
       score = 0;
     }
